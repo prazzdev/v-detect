3c6362dd580b2352a2dc1aff8e096c5ce0ac80fb
prazzdev <prazzid31@gmail.com>
2026-05-15 13:25:07 +0700

    Update++

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,39 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Radar Lalu Lintas Pintar",
+        short_name: "RadarLalin",
+        description:
+          "Peringatan persimpangan dan kendaraan terdekat secara real-time",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
